967efb64113b7c4f987ce82570ea02c598d93a0a
Clinto Abraham <clinto92@gmail.com>
2023-06-06 20:16:19 +0530

    migrated to vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,vue,jsx}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    extend: {
-      fontFamily: {
-        sans: ["Josefin Sans", "sans-serif"],
-        alata: ["Alata"],
-      },
-      letterSpacing: {
-        widest: ".3em",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
